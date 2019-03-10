be5ff063c4ddf2b32663d83ec6a97ebe0cd5529d
Ines <inesp.pino@gmail.com>
2019-03-10 16:07:49 +0100

    feature: add docs

---
+++ docs/precache-manifest.fbea458fd53a90e845ef2f3b670b6dfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "e67f61e0db8cc349c606",
+    "url": "./static/js/main.d5c01cac.chunk.js"
+  },
+  {
+    "revision": "d9837db60979621c3c0b",
+    "url": "./static/js/2.bdee699b.chunk.js"
+  },
+  {
+    "revision": "e67f61e0db8cc349c606",
+    "url": "./static/css/main.367f2f06.chunk.css"
+  },
+  {
+    "revision": "b3a54956f7a359a73dff789d8f4f9365",
+    "url": "./index.html"
+  }
+];
